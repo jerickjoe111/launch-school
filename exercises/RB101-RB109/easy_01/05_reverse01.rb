c6925e5e01cc728403763b2295157489764879ff
Lucas S <lucas.sorribes@gmail.com>
2022-06-12 22:33:42 +0200

    Add easy01/05_reverse01.rb

---
+++ exercises/RB101-RB109/easy_01/05_reverse01.rb
@@ -0,0 +1,8 @@
+def reverse_sentence(string)
+  string.split.reverse.join(" ")
+end
+
+puts reverse_sentence('Hello World') == 'World Hello'
+puts reverse_sentence('Reverse these words') == 'words these Reverse'
+puts reverse_sentence('') == ''
+puts reverse_sentence('    ') == '' # Any number of spaces results in ''
