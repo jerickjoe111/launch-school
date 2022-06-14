98b52b36db3ee8ec8cfe4c70cf117941dcaa2f7a
Lucas S <lucas.sorribes@gmail.com>
2022-06-15 01:08:25 +0200

    Refine easy_02/02_room.rb

--- exercises/RB101-RB109/easy_02/02_room.rb
+++ exercises/RB101-RB109/easy_02/02_room.rb
@@ -1,9 +1,11 @@
+METERS_TO_FEET = 10.7639
+
 puts "Enter the length of the room in meters: "
-length = gets.chomp.to_f
+length = gets.to_f
 
 puts "Enter the width of the room in meters: "
-width = gets.chomp.to_f
+width = gets.to_f
 
 area = length * width
 
-puts "The area of the room is #{area} square meters (#{area * 10.7639} square feet)."
+puts "The area of the room is #{area.round(2)} square meters (#{(area * METERS_TO_FEET).round(2)} square feet)."
