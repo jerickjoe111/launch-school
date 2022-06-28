f5b4f01dd1e05c4ea8b6834da6ae34c3668727d1
Lucas S <lucas.sorribes@gmail.com>
2022-06-28 15:29:49 +0200

    Add easy_03/01_searching.rb

---
+++ exercises/RB101-RB109/easy_03/01_searching.rb
@@ -0,0 +1,19 @@
+
+numbers_array = []
+last_number = ""
+
+6.times do |times|
+  unless times + 1 == 6
+    puts "Enter the number ##{times + 1}: "
+    numbers_array << gets.chomp.strip.to_i
+  else
+    puts "Enter the last number: "
+    last_number = gets.chomp.strip.to_i
+  end
+end
+
+if numbers_array.include?(last_number)
+  puts "The number #{last_number} appears in #{numbers_array.inspect}."
+else
+  puts "The number #{last_number} does not appear in #{numbers_array.inspect}."
+end
