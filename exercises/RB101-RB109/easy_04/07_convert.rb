40dc6448c1083717bcf74dcf6e03a08f9d0b0bdd
Lucas S <lucas.sorribes@gmail.com>
2022-06-30 15:23:14 +0200

    Format easy04/07_convert.rb

--- exercises/RB101-RB109/easy_04/07_convert.rb
+++ exercises/RB101-RB109/easy_04/07_convert.rb
@@ -2,6 +2,8 @@ ASCII_ZERO = 48
 
 def string_to_integer(string)
   digits_array = []
+
   string.chars.map { |digit| digits_array << digit.ord - ASCII_ZERO }
+  
   digits_array.inject { |accumulator, digit| accumulator * 10 + digit }
 end
