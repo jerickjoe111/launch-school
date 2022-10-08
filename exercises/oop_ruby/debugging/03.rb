026961bb43d3f611cf109599f7e2a6204448012f
Lucas S <lucas.sorribes@gmail.com>
2022-10-08 14:54:26 +0200

    Add RB120 oop debugging exercises

---
+++ exercises/oop_ruby/debugging/03.rb
@@ -0,0 +1,50 @@
+# Exercise 03
+
+# On lines 43 and 44 of our code, we can see that `grace` and `ada` are located at 
+# the same coordinates. So why does line 45 output `false`? 
+# Fix the code to produce the expected output.
+
+class Person
+  attr_reader :name
+  attr_accessor :location
+
+  def initialize(name)
+    @name = name
+  end
+
+  def teleport_to(latitude, longitude)
+    @location = GeoLocation.new(latitude, longitude)
+  end
+end
+
+class GeoLocation
+  attr_reader :latitude, :longitude
+
+  def initialize(latitude, longitude)
+    @latitude = latitude
+    @longitude = longitude
+  end
+
+  def to_s
+    "(#{latitude}, #{longitude})"
+  end
+
+  def ==(other_location)
+    to_s == other_location.to_s
+  end
+end
+
+# Example
+
+ada = Person.new('Ada')
+ada.location = GeoLocation.new(53.477, -2.236)
+
+grace = Person.new('Grace')
+grace.location = GeoLocation.new(-33.89, 151.277)
+
+ada.teleport_to(-33.89, 151.277)
+
+puts ada.location                   # (-33.89, 151.277)
+puts grace.location                 # (-33.89, 151.277)
+puts ada.location == grace.location # expected: true
+                                    # actual: false
