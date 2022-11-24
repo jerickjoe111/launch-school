a769f55a37839e7a4965408c18cc2d7e6f9ae545
Lucas S <lucas.sorribes@gmail.com>
2022-11-24 14:06:26 +0100

    Update RB130/01_blocks/assignments/todo_list.rb

--- RB130/01_blocks/assignments/todo_list.rb
+++ RB130/01_blocks/assignments/todo_list.rb
@@ -153,16 +153,3 @@ class Todo
       done == otherTodo.done
   end
 end
-
-todo1 = Todo.new("Buy milk")
-todo2 = Todo.new("Clean room")
-todo3 = Todo.new("Go to gym")
-
-list = TodoList.new("Today's Todos")
-list.add(todo1)
-list.add(todo2)
-list.add(todo3)
-
-todo1.done!
-
-binding.irb
