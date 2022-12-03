49f28f59c61dbba0a05a3454e4107c0a503dd1cb
Lucas S <lucas.sorribes@gmail.com>
2022-12-04 00:25:40 +0100

    Add ruby_more_topics/medium_01 exercises

--- exercises/ruby_more_topics/medium_01/07.rb
+++ exercises/ruby_more_topics/medium_01/07.rb
@@ -1,2 +1,41 @@
 # Exercise 07
 
+def bubble_sort!(array)
+  loop do
+    swapped = false
+    1.upto(array.size - 1) do |index|
+      if block_given?
+        next if yield(array[index - 1], array[index])
+      else
+        next if array[index - 1] <= array[index]
+      end
+
+      array[index - 1], array[index] = array[index], array[index - 1]
+
+      swapped = true
+    end
+
+    break unless swapped
+  end
+end
+
+# Further exploration:
+
+def bubble_sort_extra!(array)
+  loop do
+    swapped = false
+    1.upto(array.size - 1) do |index|
+      if block_given?
+        next if yield(array[index - 1]) <= yield(array[index])
+      else
+        next if array[index - 1] <= array[index]
+      end
+
+      array[index - 1], array[index] = array[index], array[index - 1]
+
+      swapped = true
+    end
+
+    break unless swapped
+  end
+end
