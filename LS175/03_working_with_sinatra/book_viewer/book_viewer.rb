a06a600a60114e193d1e6a5d9256bf7904c60b72
Lucas S <lucas.sorribes@gmail.com>
2023-01-25 01:22:19 +0100

    Add Book Viewer exercise to LS175/03_working_with_sinatra

--- LS175/03_working_with_sinatra/book_viewer/book_viewer.rb
+++ LS175/03_working_with_sinatra/book_viewer/book_viewer.rb
@@ -2,24 +2,76 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
-require_relative 'helpers'
+require_relative "helpers"
 
 BOOK_TITLE = 'The Adventures of Sherlock Holmes'
-TABLE_OF_CONTENTS = File.readlines('data/toc.txt')
+
+before do
+  @table_of_contents = File.readlines('data/toc.txt')
+end
+
+# View helpers:
+helpers do
+  def to_chapters(table_of_contents)
+    table_of_contents.map.with_index do |chapter, index|
+      "<li class='pure-menu-item'><a href='/chapters/#{index + 1}' class='pure-menu-link'>#{chapter}</a></li>"
+    end.join
+  end
+
+  def in_paragraphs(text)
+    text.split("\n\n").map.with_index do |paragraph, index|
+      "<p id='paragraph#{index}'%>#{paragraph}</p>"
+    end.join
+  end
+
+  def search_results(chapter_list, search_string)
+    chapter_list.map do |title, search_info|
+      chapter_number = search_info[:number]
+      "<li>"\
+        "<h4>#{title}: </h4>"\
+        "<ul>"\
+          "#{search_info[:paragraphs].map do |(number, paragraph)|
+            "<li><a href='/chapters/#{chapter_number}#paragraph#{number}'>"\
+            "<p>#{highlight(search_string, paragraph)}</p></a></li>"
+            end.join}"\
+        "</ul>"\
+      "</li>"
+    end.join
+  end
+
+  def highlight(string, paragraph)
+    original_string = paragraph[paragraph.downcase.index(string), string.size]
+
+    paragraph.gsub!(original_string, "<strong>#{original_string}</strong>")
+  end
+end
 
 get '/' do
   @title = BOOK_TITLE
-  @table_of_contents = TABLE_OF_CONTENTS
-
   erb :home
 end
 
 get '/chapters/:number' do |number|
   number = number.to_i
-  @table_of_contents = TABLE_OF_CONTENTS
+  redirect '/' unless valid_chapter?(number)
+
   @chapter_text = File.read("data/chp#{number}.txt")
   @chapter_name = @table_of_contents[number.to_i - 1].strip
   @chapter_title = "Chapter #{to_roman(number)}: #{@chapter_name}"
 
   erb :chapter
+end
+
+get '/search' do
+  unless params.empty?
+    @search_string = params[:query]
+
+    @matching_chapters = find_in_chapters(@search_string)
+  end
+
+  erb :search
+end
+
+not_found do 
+  redirect '/'
 end
--- LS175/03_working_with_sinatra/book_viewer/helpers.rb
+++ LS175/03_working_with_sinatra/book_viewer/helpers.rb
@@ -1,20 +1,52 @@
-VALUES = [
-  ["M", 1000], 
-  ["D", 500], 
-  ["C", 100], 
-  ["L", 50], 
-  ["X", 10], 
-  ["V", 5], 
-  ["I", 1], 
-]
-
 def to_roman(number)
-  roman = ''
-  VALUES.each do |pair|
-    letter = pair[0]
-    value = pair[1]
-    roman += letter*(number / value)
+  values = {
+    1000 => "M",  
+     900 => "CM",  
+     500 => "D",  
+     400 => "CD",
+     100 => "C",  
+      90 => "XC",  
+      50 => "L",  
+      40 => "XL",  
+      10 => "X",  
+        9 => "IX",  
+        5 => "V",  
+        4 => "IV",  
+        1 => "I",  
+  }
+
+  roman = ""
+  values.each do |value, letter|
+    roman << letter*(number / value)
     number = number % value
   end
   roman
+end
+
+def valid_chapter?(number)
+   (1..@table_of_contents.size).cover?(number.to_i) 
+end
+
+def find_in_chapters(string)
+  return {} if string.nil?
+
+  chapters_list = 
+    Dir.glob('data/chp*').sort_by do |chapter_file_name|
+      chapter_file_name.match(/(\d+).*+/)[1].to_i
+    end
+
+  matching_chapters = @table_of_contents.each_with_object({}) { |title, hash| hash[title] = { paragraphs: [] }  }
+
+  chapters_list.each_with_index do |chapter_file_name, index|
+    File.read(chapter_file_name).split("\n\n").each_with_index do |paragraph, paragraph_index|
+      if paragraph.downcase.include?(string.downcase)
+        matching_chapters[@table_of_contents[index]][:paragraphs] << [paragraph_index, paragraph]
+        matching_chapters[@table_of_contents[index]][:number] = index + 1
+      end
+    end
+  end
+
+  matching_chapters.reject! { |_, hash| hash[:paragraphs].empty? }
+
+  matching_chapters
 end
