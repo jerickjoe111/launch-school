d8b06c24c31848ea9ea9d7bd5f03431690242aee
Lucas S <lucas.sorribes@gmail.com>
2023-07-27 16:12:57 +0200

    Finish JS229/06_modules.md

--- JS229/main.mjs
+++
@@ -1,6 +0,0 @@
-import * as MyModule from "./module.mjs";
-
-console.log(MyModule.func()) // => 'whatever'
-console.log(MyModule.variable) // => 1
-console.log(MyModule.constant) // => 2
-console.log(MyModule.object) // => { a: 1 }
